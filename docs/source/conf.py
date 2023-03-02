2e4420363eaf2061e2807e395a848cf3a968bc3c
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2023-03-02 06:16:35 +0300

    revert back theme changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 # At the top.
-#import sphinx_bootstrap_theme
-import guzzle_sphinx_theme
+import sphinx_bootstrap_theme
+#import guzzle_sphinx_theme
 
 project = 'KnowledgeBase'
 copyright = '2023, Constantinople'
@@ -33,12 +33,12 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
-#html_theme = 'bootstrap'
-#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'sphinx_rtd_theme'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
+#html_theme_path = guzzle_sphinx_theme.html_theme_path()
+#html_theme = 'guzzle_sphinx_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
